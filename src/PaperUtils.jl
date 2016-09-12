b472f19dec78d12b7db2d6a9e60a2ec0d3c9c57c
Scott Thomas <swt30@ast.cam.ac.uk>
2016-09-12 16:36:26 +0100

    Add basic plot settings

--- src/PaperUtils.jl
+++ src/PaperUtils.jl
@@ -1,5 +1,53 @@
 module PaperUtils
 
-# package code goes here
+using Plots
+export plot, plot!, png, pdf, pyplot
+using Reexport
+@reexport using LaTeXStrings
 
-end # module
+using JLD
+
+export autofig
+export savefigdata, loadfigdata
+
+# Common plot options
+figdir = "autofigs"
+plotfont = font("Fira Sans", 10)
+titlefont = font("Fira Sans", 14)
+plotopts = Dict(
+    :linewidth=>2,
+    :legend=>true,
+    :tickfont=>plotfont,
+    :legendfont=>plotfont,
+    :guidefont=>plotfont,
+    :titlefont=>titlefont
+)
+pyplot(;plotopts...)
+PyPlot.rc("font", family="serif")
+
+"Make a figure and save it to files"
+function autofig(plotfunc, name)
+    p = plotfunc()
+    figloc = joinpath(figdir, name)
+    Plots.png(figloc)
+    Plots.pdf(figloc)
+
+    p
+end
+
+# Common data file options
+datadir = "figdata"
+
+"Save a dictionary of values into a JLD file"
+function savefigdata(id, datadict)
+    filename = joinpath(datadir, id) * ".jld"
+    save(filename, datadict)
+end
+
+"Load a dictionary of values from a JLD file"
+function loadfigdata(id)
+    filename = joinpath(datadir, id) * ".jld"
+    load(filename)
+end
+
+end # module PaperUtils
